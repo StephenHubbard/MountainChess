9990d7fdeb15c191f6870b7e41e7ea69fb2ad145
Stephen <hubbard_stephen@yaho.com>
2019-12-16 10:34:28 -0700

    chessboard component set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Chessboard from 'chessboardjsx';
+import Chess from 'chess.js';
+import Demo from "../src/Components/GameView/Chessboard/Demo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+
+
+export default class ChessExample extends Component {
+  
+  render() {
+    // random chess game
+    // var chess = new Chess();
+
+    //   while (!chess.game_over()) {
+    //     var moves = chess.moves();
+    //     var move = moves[Math.floor(Math.random() * moves.length)];
+    //     chess.move(move);
+    //   }
+    //   console.log(chess.pgn());
+    // end random chess game
+    return (
+      <div className="App">
+        <header className="App-header">
         <h1>Mountain Chess</h1>
-      </header>
-    </div>
-  );
-}
+        </header>
 
-export default App;
+        {/* <Chessboard position="start"/> */}
+        <Demo />
+        <Demo />
+      </div>
+    )
+  }
+}
