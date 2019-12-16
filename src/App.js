3006a426f916d79a09493ea0fa684ad932dd91b4
Stephen <hubbard_stephen@yaho.com>
2019-12-16 10:01:50 -0700

    about to pull master

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Mountain Poker</h1>
+        <h1>Mountain Chess</h1>
       </header>
     </div>
   );
