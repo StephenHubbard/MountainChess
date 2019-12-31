eaf8553124a0ce521ee6407e4efd9bace65ce859
Robert Reiss <reissaudiodesign@yahoo.com>
2019-12-31 10:08:04 -0800

    added contact info for Rob, Lance and Carter

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,9 +9,9 @@ export default class About extends Component {
     render() {
         return (
         <div className="about-page">
-            <h1>About Page</h1>
+            <h1>About Mountain Chess</h1>
             
-            <h3> Mountain Chess was a project built using React by four students at Dev Mountain in Lehi Utah, as part of a group project.  It was built using React, Nodejs, PostgreSQL, javascript, socketio, and other misc techs.  Not only did we build our own gaming engine, but we built a full stack application user interface.  Integrating users the ability to see other online users, a leaderboard, challenging and chatting with other users, adding friend functionality, and more. If you would like to learn more about the project, please don't hesitate to contact any of us here.</h3>            
+            <h3> Mountain Chess is a full stack web application built by a small team of developers using React, Node.js, PostgreSQL, Javascript, Socket.IO and other technologies.  Mountain Chess features its own game engine constructed from scratch, as well as many different features that are all integrated into a fully-functional user interface.  Players can add friends, see when those friends are online, chat with other users and view a leaderboard that displays the top-performing users on the website. If you would like to learn more about the project, you can contact any of us by using the links below.</h3>            
             <></>
             <div className="names-cont">
                 <div className="name">
@@ -28,18 +28,30 @@ export default class About extends Component {
                 </div>
                 <div className="name">
                     <h2>Rob Reiss:</h2>
+                    <a href="https://www.linkedin.com/in/robertcreiss/">
                     <img className="linkedin-logo" src="https://icons-for-free.com/iconfiles/png/512/linkedin+linkedin+logo+networking+social+media+icon-1320196081476022403.png" alt=""/>
+                    </a>
+                    <a href="https://github.com/RobertReiss">
                     <img className="github-logo" src="http://pngimg.com/uploads/github/github_PNG83.png" alt=""/>
+                    </a>
                 </div>
                 <div className="name">
                     <h2>Lance Jenkins:</h2>
+                    <a href="https://www.linkedin.com/in/lance-jenkins/">
                     <img className="linkedin-logo" src="https://icons-for-free.com/iconfiles/png/512/linkedin+linkedin+logo+networking+social+media+icon-1320196081476022403.png" alt=""/>
+                    </a>
+                    <a href="https://github.com/laprje">
                     <img className="github-logo" src="http://pngimg.com/uploads/github/github_PNG83.png" alt=""/>
+                    </a>
                 </div>
                 <div className="name">
                     <h2>Carter Endsley:</h2>
+                    <a href="https://www.linkedin.com/in/carterendsley/">
                     <img className="linkedin-logo" src="https://icons-for-free.com/iconfiles/png/512/linkedin+linkedin+logo+networking+social+media+icon-1320196081476022403.png" alt=""/>
+                    </a>
+                    <a href="https://github.com/carterqe">
                     <img className="github-logo" src="http://pngimg.com/uploads/github/github_PNG83.png" alt=""/>
+                    </a>
                 </div>
             </div>
         </div>
